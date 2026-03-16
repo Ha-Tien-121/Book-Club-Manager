efb5f5e3b7bd00836fe3ed9fae09eed264284337
Elsie Wang <elsiewang7@gmail.com>
2026-03-15 20:06:47 -0700

    Refactor app to one folder

--- data/scripts/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-
-DATA_DIR = os.path.join(BASE_DIR, 'data')
-
-RAW_DIR = os.path.join(DATA_DIR, 'raw')
-PROCESSED_DIR = os.path.join(DATA_DIR, 'processed')
